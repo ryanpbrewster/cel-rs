a0b80376dc62d649138ca06b45a5ea0d879072a3
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-12-16 21:06:22 -0800

    Executable that parses from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate cel_rs;
+
+use std::io;
+use std::io::Read;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+fn main() -> Result<()> {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf)?;
+    let parsed = cel_rs::parsers::parse(&buf)?;
+    println!("{:?}", parsed);
+    Ok(())
 }
--- src/parsers.rs
+++ src/parsers.rs
@@ -1,22 +1,31 @@
+use crate::model::Expression;
+
 lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
 lalrpop_mod!(pub calculator3); // synthesized by LALRPOP
 lalrpop_mod!(pub cel); // synthesized by LALRPOP
 
+pub fn parse(input: &str) -> Result<Expression, String> {
+    cel::ExprParser::new()
+        .parse(input)
+        .map_err(|err| format!("{:?}", err))
+}
+
 #[cfg(test)]
 mod test {
-    use crate::model::{Expression, Literal};
+    use super::*;
+    use crate::model::Literal;
 
     #[test]
     fn smoke1() {
         let input = "22";
-        let res = assert_eq!(super::calculator1::TermParser::new().parse(input), Ok(22));
+        let res = assert_eq!(calculator1::TermParser::new().parse(input), Ok(22));
     }
 
     #[test]
     fn smoke3() {
         let input = "22 * (4 + 15)";
         let res = assert_eq!(
-            super::calculator3::ExprParser::new().parse(input),
+            calculator3::ExprParser::new().parse(input),
             Ok(22 * (4 + 15))
         );
     }
@@ -25,7 +34,7 @@ mod test {
     fn cel_smoke() {
         let input = "22 * (4 + 15)";
         let res = assert_eq!(
-            super::cel::ExprParser::new().parse(input),
+            cel::ExprParser::new().parse(input),
             Ok(Expression::Mul(vec![
                 Expression::Lit(Literal::I64(22)),
                 Expression::Add(vec![
@@ -40,7 +49,7 @@ mod test {
     fn cel_list() {
         let input = "[0, 1, 2]";
         let res = assert_eq!(
-            super::cel::ExprParser::new().parse(input),
+            cel::ExprParser::new().parse(input),
             Ok(Expression::Lit(Literal::List(vec![
                 Expression::Lit(Literal::I64(0)),
                 Expression::Lit(Literal::I64(1)),

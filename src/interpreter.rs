59cee2f46b029af7d71886083902b8693a685a5f
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-12-21 20:21:01 -0800

    check for some type errors and divide-by-zero errors

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -49,7 +49,13 @@ pub fn evaluate(expr: Expression) -> EvalResult {
             let a = evaluate(*a)?;
             let b = evaluate(*b)?;
             match (a, b) {
-                (Literal::I64(a), Literal::I64(b)) => Ok(Literal::I64(a / b)),
+                (Literal::I64(a), Literal::I64(b)) => {
+                    if b != 0 {
+                        Ok(Literal::I64(a / b))
+                    } else {
+                        Err(String::from("divide by zero"))
+                    }
+                }
                 (Literal::F64(a), Literal::F64(b)) => Ok(Literal::F64(a / b)),
                 _ => Err(String::from("invalid types")),
             }
@@ -73,31 +79,43 @@ mod test {
 
     #[test]
     fn smoke() {
+        let input = r#" 1 + 2 + 3 + 4 + 5 "#;
         assert_eq!(
-            evaluate(
-                parse(
-                    r#"
-                1 + 2 + 3 + 4 + 5
-            "#
-                )
-                .unwrap()
-            ),
+            evaluate(parse(input).unwrap()),
             Ok(Literal::I64(1 + 2 + 3 + 4 + 5)),
         );
     }
 
     #[test]
     fn string_addition() {
+        let input = r#" "asdf" + "pqrs" + "tuvw" == "asdfpqrstuvw" "#;
+        assert_eq!(evaluate(parse(input).unwrap()), Ok(Literal::Bool(true)),);
+    }
+
+    #[test]
+    fn type_error_adding_string_and_int() {
+        let input = r#" "asdf" + 5 "#;
         assert_eq!(
-            evaluate(
-                parse(
-                    r#"
-                "asdf" + "pqrs" + "tuvw" == "asdfpqrstuvw"
-            "#
-                )
-                .unwrap()
-            ),
-            Ok(Literal::Bool(true)),
+            evaluate(parse(input).unwrap()),
+            Err(String::from("invalid types"))
+        );
+    }
+
+    #[test]
+    fn type_error_subtracting_strings() {
+        let input = r#" "asdf" - "pqrs" "#;
+        assert_eq!(
+            evaluate(parse(input).unwrap()),
+            Err(String::from("invalid types"))
+        );
+    }
+
+    #[test]
+    fn eval_error_divide_by_zero_int() {
+        let input = r#" 1 / 0 "#;
+        assert_eq!(
+            evaluate(parse(input).unwrap()),
+            Err(String::from("divide by zero"))
         );
     }
 }
